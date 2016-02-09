366b620745d4b7c06844c368b7224f6075caba10
Matt Morris <mcm0027@yahoo.com>
2016-02-09 10:43:11 -0600

    add check toggle functionality through Angular

--- js/app.js
+++ js/app.js
@@ -25,6 +25,24 @@ myApp.factory('items', function() {
     items.push({name: newItem, checked: false});
     return items;
   };
+
+  items.clearList = function() {
+    items.length = 0;
+   return items;
+  };
+
+  items.toggleCheck = function(itemName) {
+    var lookup = {};
+    for (var i = 0; i < items.length; i++) {
+      lookup[items[i].name] = items[i];
+    }
+    var itemCheck = lookup[itemName];
+    if (itemCheck.checked) {
+      itemCheck.checked = false;
+    } else {
+      itemCheck.checked = true;
+    }
+  };
   return items;
 });
 
@@ -32,6 +50,20 @@ myApp.controller('listController', ['$scope', 'items', function($scope, items) {
   $scope.items = items.set();
   $scope.addItem = function(newItem) {
     items.addItem(newItem);
+  };
+
+  $scope.clear = function () {
+    items.clearList();
+  };
+
+  $scope.enterEvent = function(event, newItem) {
+    if (event.which === 13) {
+    items.addItem(newItem);
+    }
+  };
+
+  $scope.check = function(name) {
+      items.toggleCheck(name);
   };
 }]);
 
@@ -60,30 +92,30 @@ $(document).ready(function() {
 	// 	$('#add-button').prev().val('');
 	// });
 
-	$("#clear-button").click(function() {
-		$("ul").children().remove();
-	});
+	// $("#clear-button").click(function() {
+	// 	$("ul").children().remove();
+	// });
 
-	$(document).keydown(function(e){
-		if((e.which == 13) && $("#add-button").prev().is(":focus")) {
-			$(".unchecked").append('<li><input type="checkbox" class="list-checkbox"> <p class="unstrike">' + $("#add-button").prev().val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
-			$('#add-button').prev().val('');
-		}
-	});
+	// $(document).keydown(function(e){
+	// 	if((e.which == 13) && $("#add-button").prev().is(":focus")) {
+	// 		$(".unchecked").append('<li><input type="checkbox" class="list-checkbox"> <p class="unstrike">' + $("#add-button").prev().val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
+	// 		$('#add-button').prev().val('');
+	// 	}
+	// });
 
-	$("ul").on("click", ".list-checkbox", function() {
-		if ($(this).parent().parent().attr("class") == "unchecked ui-sortable") {
-				console.log($(".list-checkbox").parent().parent().attr("class"));
-			$(".checked").append($(this).parent());
-				$(this).next().removeClass("unstrike");
-				$(this).next().addClass("strike");
-			} else {
-				console.log("hello");
-				$(".unchecked").append($(this).parent());
-				$(this).next().removeClass("strike");
-				$(this).next().addClass("unstrike");
-			}
-		});
+	// $("ul").on("click", ".list-checkbox", function() {
+	// 	if ($(this).parent().parent().attr("class") == "unchecked ui-sortable") {
+	// 			console.log($(".list-checkbox").parent().parent().attr("class"));
+	// 		$(".checked").append($(this).parent());
+	// 			$(this).next().removeClass("unstrike");
+	// 			$(this).next().addClass("strike");
+	// 		} else {
+	// 			console.log("hello");
+	// 			$(".unchecked").append($(this).parent());
+	// 			$(this).next().removeClass("strike");
+	// 			$(this).next().addClass("unstrike");
+	// 		}
+	// 	});
 
 	$("ul").on("click", ".remove", function() {
 		$(this).parent().remove();
