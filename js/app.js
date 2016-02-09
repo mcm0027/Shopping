acea7770aac920dce2d54f69b8068e874de42af3
Matt Morris <mcm0027@yahoo.com>
2016-02-08 21:26:07 -0600

    add typings and bootstrap angular

--- js/app.js
+++ js/app.js
@@ -1,24 +1,34 @@
-function enterKeydown(elementClass, deleteInput) {
-	$(document).keydown(function(e){ 
-			
-		if((e.which == 13) && $(elementClass).is(":focus")) {
-			$(elementClass).parent().parent().append('<li><input type="checkbox" class="list-checkbox grabcheckbox"> <p class="unstrike grabtext">' + $(elementClass).val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
-			if ($(".addbutton").parent().parent().attr("class") == "checked ui-sortable") {
-				$(".grabtext").removeClass("unstrike").addClass("strike");
-				$(".grabcheckbox").attr('checked', 'checked');		
-			}
-			$(".grabtext").removeClass("grabtext");
-			$(".grabcheckbox").removeClass("grabcheckbox");
-			if (deleteInput = true){
-				$(elementClass).parent().remove();
-			}
-		}
-	});
-}
+var myApp = angular.module("myApp", ['ui.sortable']);
+
+myApp.factory('uncheckedItems', [ function(
+  
+
+)]);
+
+myApp.controller('listController', function() {
+
+
+});
+
+// function enterKeydown(elementClass, deleteInput) {
+// 	$(document).keydown(function(e){
+
+// 		if((e.which == 13) && $(elementClass).is(":focus")) {
+// 			$(elementClass).parent().parent().append('<li><input type="checkbox" class="list-checkbox grabcheckbox"> <p class="unstrike grabtext">' + $(elementClass).val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
+// 			if ($(".addbutton").parent().parent().attr("class") == "checked ui-sortable") {
+// 				$(".grabtext").removeClass("unstrike").addClass("strike");
+// 				$(".grabcheckbox").attr('checked', 'checked');
+// 			}
+// 			$(".grabtext").removeClass("grabtext");
+// 			$(".grabcheckbox").removeClass("grabcheckbox");
+// 			if (deleteInput = true){
+// 				$(elementClass).parent().remove();
+// 			}
+// 		}
+// 	});
+// }
 
 $(document).ready(function() {
-	$( ".unchecked" ).sortable();
-	$( ".checked" ).sortable();
 
 	$("#add-button").click(function() {
 		$(".unchecked").append('<li><input type="checkbox" class="list-checkbox"> <p class="unstrike">' + $(this).prev().val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
@@ -29,7 +39,7 @@ $(document).ready(function() {
 		$("ul").children().remove();
 	});
 
-	$(document).keydown(function(e){ 
+	$(document).keydown(function(e){
 		if((e.which == 13) && $("#add-button").prev().is(":focus")) {
 			$(".unchecked").append('<li><input type="checkbox" class="list-checkbox"> <p class="unstrike">' + $("#add-button").prev().val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
 			$('#add-button').prev().val('');
@@ -59,12 +69,12 @@ $(document).ready(function() {
 		$(this).parent().parent().append('<li><input type="text" class="editinput" value="' + item + '"><input type="button" class="button addbutton pull-right" value="Add"></li>');
 		$(".editinput").focus();
 		$(this).parent().remove();
-		
+
 	});
 
 	$("ul").on("click", ".addbutton", function() {
 		$(this).parent().parent().append('<li><input type="checkbox" class="list-checkbox grabcheckbox"> <p class="unstrike grabtext">' + $(this).prev().val() + '</p> <input type="button" class="button edit" value="Edit"> <input type="button" class="button remove" value="Remove"></li>');
-		
+
 		if ($(".addbutton").parent().parent().attr("class") == "unchecked ui-sortable") {
 			console.log("HI")
 			$(".grabtext").removeClass("grabtext");
@@ -81,6 +91,6 @@ $(document).ready(function() {
 
 		});
 
-		enterKeydown(".editinput", true); 
+		enterKeydown(".editinput", true);
 	});
 
